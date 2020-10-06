c6fc5d5edfbc4454c4165af9032ccf0a9f0aa40b
luis_arboleda17 <hello@larboleda.io>
2020-10-06 18:15:02 -0500

    Added db connection to e2e tests

--- test/airports.e2e-spec.ts
+++ test/airports.e2e-spec.ts
@@ -1,48 +1,89 @@
+
 import { Test, TestingModule } from '@nestjs/testing';
 import { INestApplication } from '@nestjs/common';
 import * as request from 'supertest';
+
 import { AppModule } from '../src/app.module';
-import { AirportsService } from '../src/airports/airports.service';
+import { AirportDTO } from '../src/models/airport.dto';
+import { getRepositoryToken } from '@nestjs/typeorm';
+import { Airport } from '../src/models/airport.entity';
+import { Repository } from 'typeorm';
+
+const AIRPORTS_EXAMPLES = [
+  {
+    id: '1',
+    name: 'Goroka Airport',
+    city: 'Goroka',
+    country: 'Papua New Guinea',
+    IATA: 'GKA',
+    ICAO: 'AYGA',
+    latitude: '-6.08168983459',
+    longitude: '145.391998291',
+    altitude: '5282',
+    timezone: 10,
+    dst: 'U',
+    type: 'airport',
+    source: 'OurAirports'
+  },
+  {
+    id: '2',
+    name: 'Madang Airport',
+    city: 'Madang',
+    country: 'Papua New Guinea',
+    IATA: 'MAG',
+    ICAO: 'AYMD',
+    latitude: '-5.20707988739',
+    longitude: '145.789001465',
+    altitude: '20',
+    timezone: 10,
+    dst: 'U',
+    type: 'airport',
+    source: 'OurAirports',
+  },
+];
 
 describe('Airports (e2e)', () => {
   let app: INestApplication;
-  const airportsService = {
-    findAll: () => [{id: 1}, {id: 1}],
-    find: (id: string) => id === '1' ? {id: 1} : null,
-  };
+  let airportRepository: Repository<Airport>;
 
   beforeEach(async () => {
     const moduleFixture: TestingModule = await Test.createTestingModule({
       imports: [AppModule],
-    }).overrideProvider(AirportsService)
-      .useValue(airportsService)
-      .compile();
+    }).compile();
 
     app = moduleFixture.createNestApplication();
+    airportRepository = moduleFixture.get(getRepositoryToken(Airport));
     await app.init();
   });
 
-  it('Fetch all airports, then receive all airports', () => {
+  afterEach(async () => {
+    await airportRepository.query(`DELETE FROM ${airportRepository.metadata.tableName}`);
+    await app.close();
+  });
+
+  it('Fetch all airports, then receive all airports', async () => {
+    await airportRepository.save(AIRPORTS_EXAMPLES.map(airport => AirportDTO.from(airport).toEntity()));
     return request(app.getHttpServer())
       .get('/airports')
       .expect(200)
       .expect({
-        data: airportsService.findAll(),
+        data: AIRPORTS_EXAMPLES,
       });
   });
 
-  it('Fetch and airport by it\'s id, then receive an airport', () => {
+  it('Fetch and airport by it\'s id, then receive an airport', async () => {
+    await airportRepository.save([AirportDTO.from(AIRPORTS_EXAMPLES[0]).toEntity()]);
     return request(app.getHttpServer())
-      .get('/airports/1')
+      .get(`/airports/${AIRPORTS_EXAMPLES[0].id}`)
       .expect(200)
       .expect({
-        data: airportsService.find('1'),
+        data: AIRPORTS_EXAMPLES[0],
       });
   });
 
   it('Fetch and airport by it\'s id, then throw an error if it is not found', () => {
     return request(app.getHttpServer())
-      .get('/airports/2')
+      .get('/airports/test-id')
       .expect(404);
   });
 });
